00a279b3c438daaaee34100fd952129cacf52381
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2015-04-27 21:10:30 +0200

    Creating init.el. Starting Emacs in server mode

---
+++ init.el
@@ -0,0 +1,13 @@
+;;;
+;;; Sandor Juhasz' init.el
+;;;
+;;; These are my Emacs settings I use for Clojure development and
+;;; various other tasks using Emacs.
+;;;
+
+;;
+;; General emacs settings to integrate the tool into my Windows
+;; toolchain
+;;
+(server-start)
+
